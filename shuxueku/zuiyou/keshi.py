2c1503bb741d48682abed859e1821b6eba8f92d2
conanxu1 <872144755@qq.com>
2018-11-01 02:06:22 -0400

    new2

--- shuxueku/zuiyou/keshi.py
+++ shuxueku/zuiyou/keshi.py
@@ -1,11 +1,15 @@
 import os
 import numpy as np
-
+from pyvirtualdisplay import Display
+import matplotlib.pyplot as plt
 
 f=open('log.txt','r',encoding='utf-8')
 
-w=f.readlines()
+display = Display(visible=0, size=(800, 600))
+display.start()
 
+w=f.readlines()
+f.close()
 g=0
 n=np.shape(w);
 print(n[0])
@@ -13,5 +17,14 @@ print(n[0])
 
 
 x=[]
-for i in range(0,int(n/3)):
+t=[]
 
+for i in range(0,int(n[0]/3)):
+	x.append(float(w[i*3].split(',')[0]))
+	t.append(float(w[i*3].split(',')[1]))
+
+
+plt.plot(t,x)
+
+print(6)
+plt.savefig("3.jpg")
