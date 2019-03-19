86fb50db2aed3c36aaa336d22de29fdf568d6e3c
conanxu1 <872144755@qq.com>
2019-03-19 11:51:07 +0800

    new2

--- shuxueku/zuiyou/hello.c
+++ shuxueku/zuiyou/hello.c
@@ -29,9 +29,15 @@ void matrix_vector();
 
 void main()
 {
-double complex a=1+8*I,b=1+8*I;	
-printf("%1.1f,%1.1f",creal(a+b),cimag(a*b));
+double complex a=1+8*I,b=1-8*I;	
+printf("%1.1f,%1.1f\n\n\n",cimag(a+b),creal(a*b));
+
+while(1)
+{
+	scanf("\n");
 	
+}
+
 	/*
 	double *y,x0[3]={1,1,1},*p;
 	
