11be0d7006d9805ac7c5a5ef8e05fd8612182fcf
conanxu1 <872144755@qq.com>
2019-03-21 14:22:59 +0800

    new2

--- shuxueku/zuiyou/myfun.c
+++ shuxueku/zuiyou/myfun.c
@@ -2258,7 +2258,10 @@ shuchud(GI,dim,dim);
 //求逆
 
 
-cblas_dgemm(CblasRowMajor, CblasNoTrans,CblasNoTrans, dim, e,dim, 1,GI, dim,A, e,0,TEM2,e );	
+cblas_dgemm(CblasRowMajor, CblasNoTrans,CblasNoTrans, dim, e,dim, 1,GI, dim,A, e,0,TEM2,e );
+
+shuchud(TEM2,dim,e);
+	
 cblas_dgemm(CblasRowMajor, CblasTrans,CblasNoTrans, e, dim,dim, 1,A, e,TEM2, e,0,TEM3,e );	
 
 memcpy(V, TEM3, e*e*sizeof(double));
