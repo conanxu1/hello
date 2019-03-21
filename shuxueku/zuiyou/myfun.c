b806be13047e90368f589e287277dfecd3055654
conanxu1 <872144755@qq.com>
2019-03-21 14:58:22 +0800

    new2

--- shuxueku/zuiyou/myfun.c
+++ shuxueku/zuiyou/myfun.c
@@ -2319,7 +2319,7 @@ double *bw=(double *)malloc(e*sizeof(double));
 cblas_dgemm(CblasRowMajor, CblasTrans,CblasNoTrans, e, 1,dim, -1,A, e,u, e,1,bw,1 );	
 
 
-shuchu(w,dim,1);
+shuchud(w,dim,1);
 
 //bw v lam
 LAPACKE_dgesv(LAPACK_ROW_MAJOR,e,1,LW,e,ipive,bw,1);
