2f30dbbeb5fc0a8dc3a11499d5bcb2713f5f531c
conanxu1 <872144755@qq.com>
2019-03-21 19:58:51 +0800

    new2

--- shuxueku/zuiyou/myfun.c
+++ shuxueku/zuiyou/myfun.c
@@ -2188,7 +2188,7 @@ for(int i=0;i<ie;i++)
 			{	qinum+=1
 				A0[qinum-1]=i;
 				for(int tt;tt<dim;tt++)
-				{zuoyong[dim*e+(qinum-1)*dim+tt]=Ai[dim*i+tt]}
+				{zuoyong[dim*e+(qinum-1)*dim+tt]=Ai[dim*i+tt];}
 				
 			}
 }
