d3a0736555dca8d68d0075452e56054b8e1998c6
conanxu1 <872144755@qq.com>
2019-03-23 15:19:20 +0800

    new2

--- shuxueku/zuiyou/hello.c
+++ shuxueku/zuiyou/hello.c
@@ -28,30 +28,59 @@ void matrix_vector();
 
 
 int main()
-{	
+{
+
+/*	
 int d=3;
 
 //注意是否有1/2*G
-double H[9]={1,0,0,0,1,0,0,0,1};
+
+double H[4]={1,0,0,1};
 				
-double h[3]={0,0,0};
+double h[2]={0,0};
 
 
 //注意约束的写法ATx=b
 //改为Ax=b 可以有效增减约束
  
-double Ae[1*3]={1,2,-1};
+double Ae[1*2]={1,2};
 double be[1]={4};
 
 
 
-double Ai[3]={2,2,2};
+double Ai[2]={2,2};
 				
 double bi[1]={-1};
 
-qxt(H,h,be,Ae,bi,Ai,3,1,1);
+qxt(H,h,be,Ae,bi,Ai,2,1,1);
 
 
+*/
+
+int dim=3;
+
+//注意是否有1/2*G
+
+double H[9]={  1,0,0,
+		0,1,0,
+		0,0,160};
+
+double zuoyong[3*3]={   1,2,1,
+			-1,-2,1,
+			0,0,1};
+double tb[3]={0};
+double tg[3]={1.6,3.2,80};
+
+
+double *AM;
+
+xxwg(AM,zuoyong,3,3);
+
+
+//myqp(H,zuoyong,tg,tb,dim,3);
+
+
+//shuchud(tg,dim,1);
 
 
 
