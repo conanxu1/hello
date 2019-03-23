96ef9e95ca21dcd09e77b9f937a6fa27e53b4acf
conanxu1 <872144755@qq.com>
2019-03-23 18:02:55 +0800

    new2

--- shuxueku/zuiyou/hello.c
+++ shuxueku/zuiyou/hello.c
@@ -57,66 +57,66 @@ qxt(H,h,be,Ae,bi,Ai,2,1,1);
 
 */
 
-// int dim=3;
+int dim=3;
 
-// //注意是否有1/2*G
+//注意是否有1/2*G
 
-// double H[9]={  1,0,0,
-		// 0,1,0,
-		// 0,0,160};
+double H[9]={  1,0,0,
+		0,1,0,
+		0,0,160};
 
-// double zuoyong[3*3]={   1,2,1,
-			// -1,-2,1,
-			// 0,0,1};
-// double tb[3]={0,0,0};
-// double tg[3]={1.6,3.2,80};
-
-
-// double *AM;
-
-// //xxwg(AM,zuoyong,3,3);
-
-// myqp(H,zuoyong,tg,tb,dim,3);
+double zuoyong[3*3]={   1,2,1,
+			-1,-2,1,
+			0,0,1};
+double tb[3]={0,0,0};
+double tg[3]={1.6,3.2,80};
 
 
-// //shuchud(tg,dim,1);
+double *AM;
+
+//xxwg(AM,zuoyong,3,3);
+
+myqp(H,zuoyong,tg,tb,dim,3);
 
 
+//shuchud(tg,dim,1);
 
 
 
 
-int m=3,n=3;
 
 
-double TEM[9]={1,2,1,
-				-1,-2,1,
-				0,0,1};
+// int m=3,n=3;
 
-	int matrix_order = LAPACK_ROW_MAJOR;
-	char jobu = 'N';
-	char jobvt = 'N';
+
+// double TEM[9]={1,2,1,
+				// -1,-2,1,
+				// 0,0,1};
+
+	// int matrix_order = LAPACK_ROW_MAJOR;
+	// char jobu = 'N';
+	// char jobvt = 'N';
 	
-	int ff=m,lda=n;
+	// int ff=m,lda=n;
 
 
-	double s[ff];
+	// double s[ff];
 
 
 		
-	double u[100];
-	int ldu = m;
-	double vt[100];
-	int ldvt = n;
+	// double u[100];
+	// int ldu = m;
+	// double vt[100];
+	// int ldvt = n;
 	
 	
-	int ot=(m+n)/2+abs(m-n)/2;
-	double superb[100];
+	// int ot=(m+n)/2+abs(m-n)/2;
+	// double superb[100];
 	
 	
-	printf("ok\n");
- shuchud(TEM,m,n);
-	LAPACKE_dgesvd(matrix_order,jobu, jobvt, m, n, TEM,lda, s, u, ldu, vt, ldvt, superb);
+	// printf("ok\n");
+ // shuchud(TEM,m,n);
+	// LAPACKE_dgesvd(matrix_order,jobu, jobvt, m, n, TEM,lda, s, u, ldu, vt, ldvt, superb);
  	
 
 
