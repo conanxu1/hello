6584f5a8575bf2feafb6c481d4d7cb7445aa71f8
conanxu1 <872144755@qq.com>
2019-06-09 11:06:57 +0800

    new2

--- shuxueku/zuiyou/kongzhidemo.c
+++ shuxueku/zuiyou/kongzhidemo.c
@@ -19,35 +19,33 @@ int main()
 printf("\n\n...............\n");
  
 
-lyRnR *kkzcek,*kkzcik,*kkzphi,*kkzpsi;
-int kkznumcek=0,kkznumcik=2,kkznumphii=2, kkznumpsii=0;
-piandao kkzgPHI,kkzgg,kkzgf,*kkzgcek, *kkzgphi,*kkzgcik,*kkzgpsi,
+// lyRnR *kkzcek,*kkzcik,*kkzphi,*kkzpsi;
+// int kkznumcek=0,kkznumcik=2,kkznumphii=2, kkznumpsii=0;
+// piandao kkzgPHI,kkzgg,kkzgf,*kkzgcek, *kkzgphi,*kkzgcik,*kkzgpsi,
 
-int geshu;
+// int geshu;
 
-int dimx=3,dimu=1;
-int Ntau=3
-double *tauk,*wk,*Dki,*zuiyouX;
-
-
-
-kkzcek=malloc(kznumcek*sizeof(lyRnR));
-kkzcik=malloc(kznumcik*sizeof(lyRnR));
-kkzphi=malloc(kznumphii*sizeof(lyRnR));
-kkzpsi=malloc(kznumpsii*sizeof(lyRnR));
-kkzgcek=malloc(kznumcek*sizeof(piandao));
-kkzgphi=malloc(kznumphii*sizeof(piandao));
-kkzgcik=malloc(kznumcik*sizeof(piandao));
-kkzgpsi=malloc(kznumpsii*sizeof(piandao));
+// int dimx=3,dimu=1;
+// int Ntau=3
+// double *tauk,*wk,*Dki,*zuiyouX;
 
 
-kkzcik[0]=kzcik1;
-kkzcik[1]=kzcik2;
 
-kkzphi[0]=kzphi1;
-kkzphi[1]=kzphi2;
+// kkzcek=malloc(kznumcek*sizeof(lyRnR));
+// kkzcik=malloc(kznumcik*sizeof(lyRnR));
+// kkzphi=malloc(kznumphii*sizeof(lyRnR));
+// kkzpsi=malloc(kznumpsii*sizeof(lyRnR));
+// kkzgcek=malloc(kznumcek*sizeof(piandao));
+// kkzgphi=malloc(kznumphii*sizeof(piandao));
+// kkzgcik=malloc(kznumcik*sizeof(piandao));
+// kkzgpsi=malloc(kznumpsii*sizeof(piandao));
 
 
+// kkzcik[0]=kzcik1;
+// kkzcik[1]=kzcik2;
+
+// kkzphi[0]=kzphi1;
+// kkzphi[1]=kzphi2;
 
 
 
@@ -58,115 +56,117 @@ kkzphi[1]=kzphi2;
 
 
 
-double *h,*be,*Ae*bi,*Ai,*XUk,tk,*x0,t0;
+
+
+// double *h,*be,*Ae*bi,*Ai,*XUk,tk,*x0,t0;
 
 
 
 
 
 
-x0=(double *)malloc((dimx)*sizeof(double));
-x0[0]=0;
-x0[1]=1;
-x0[2]=0;
+// x0=(double *)malloc((dimx)*sizeof(double));
+// x0[0]=0;
+// x0[1]=1;
+// x0[2]=0;
 
 
 
-t0=0;
-
-
-
-
-
-
-tauk=(double *)malloc((Ntau)*sizeof(double));
-tk=(double *)malloc((Ntau)*sizeof(double));
-
-
-
-wk=(double *)malloc((Ntau)*sizeof(double));
-Dki=(double *)malloc((Ntau*Ntau)*sizeof(double));
-lgd(tauk,Ntau-1);
-lg_AD(tauk, Ak,Dki,Ntau-1);
+// t0=0;
 
 
 
 
-zuiyouX=(double *)malloc(((dimx+dimu)*Ntau+1)*sizeof(double));
+
+
+// tauk=(double *)malloc((Ntau)*sizeof(double));
+// tk=(double *)malloc((Ntau)*sizeof(double));
 
 
 
-geshu=(dimx+dimu)*Ntau+1;
-h=(double *)malloc(geshu*sizeof(double));
-
-geshu=kznumcek*Ntau+kznumphii+dimx*Ntau;
-be=(double *)malloc(geshu*sizeof(double));
-
-
-geshu=(kznumcek*Ntau+kznumphii+dimx*Ntau)*((dimx+dimu)*Ntau+1);
-Ae=(double *)malloc(geshu*sizeof(double));
-
-geshu=kznumcik*Ntau+kznumpsii;
-bi=(double *)malloc(geshu*sizeof(double));
-
-geshu=(kznumcik*Ntau+kznumpsii)*((dimx+dimu)*Ntau+1);
-Ai=(double *)malloc(geshu*sizeof(double));
+// wk=(double *)malloc((Ntau)*sizeof(double));
+// Dki=(double *)malloc((Ntau*Ntau)*sizeof(double));
+// lgd(tauk,Ntau-1);
+// lg_AD(tauk, Ak,Dki,Ntau-1);
 
 
 
-geshu=(dimx+dimu)*Ntau+1;
-XUk=(double *)malloc(((dimx+dimu)*Ntau+1)*sizeof(double));
+
+// zuiyouX=(double *)malloc(((dimx+dimu)*Ntau+1)*sizeof(double));
+
+
+
+// geshu=(dimx+dimu)*Ntau+1;
+// h=(double *)malloc(geshu*sizeof(double));
+
+// geshu=kznumcek*Ntau+kznumphii+dimx*Ntau;
+// be=(double *)malloc(geshu*sizeof(double));
+
+
+// geshu=(kznumcek*Ntau+kznumphii+dimx*Ntau)*((dimx+dimu)*Ntau+1);
+// Ae=(double *)malloc(geshu*sizeof(double));
+
+// geshu=kznumcik*Ntau+kznumpsii;
+// bi=(double *)malloc(geshu*sizeof(double));
+
+// geshu=(kznumcik*Ntau+kznumpsii)*((dimx+dimu)*Ntau+1);
+// Ai=(double *)malloc(geshu*sizeof(double));
+
+
+
+// geshu=(dimx+dimu)*Ntau+1;
+// XUk=(double *)malloc(((dimx+dimu)*Ntau+1)*sizeof(double));
  
 
 
-xishu(kzPHI, kzg,kzf,kkzcek,kkznumcek,kkzphii,kkznumphii,kkzcik,
-kkznumcik,kkzpsii,kkznumpsii,kkzgPHI,kkzgg,kkzgf,kkzgcek,kkzgphi,kkzgcik,kkzgpsi,
-dimx,dimu,Ntau,tauk,wk, Dki,zuiyouX ,h,be,Ae, bi, Ai,XUk, tk,x0,t0)
+// xishu(kzPHI, kzg,kzf,kkzcek,kkznumcek,kkzphii,kkznumphii,kkzcik,
+// kkznumcik,kkzpsii,kkznumpsii,kkzgPHI,kkzgg,kkzgf,kkzgcek,kkzgphi,kkzgcik,kkzgpsi,
+// dimx,dimu,Ntau,tauk,wk, Dki,zuiyouX ,h,be,Ae, bi, Ai,XUk, tk,x0,t0)
 
 
 
 
-printf("\n\n...............\n");
-geshu=(dimx+dimu)*Ntau+1;
-shuchud(h,geshu,1)
+// printf("\n\n...............\n");
+// geshu=(dimx+dimu)*Ntau+1;
+// shuchud(h,geshu,1)
  
  
-printf("\n\n...............\n");
+// printf("\n\n...............\n");
  
-geshu=kznumcek*Ntau+kznumphii+dimx*Ntau;
-shuchud(be,geshu,1);
+// geshu=kznumcek*Ntau+kznumphii+dimx*Ntau;
+// shuchud(be,geshu,1);
 
 
 
 
 
-printf("\n\n...............\n");
+// printf("\n\n...............\n");
  
-shuchud(Ae,(kznumcek*Ntau+kznumphii+dimx*Ntau),((dimx+dimu)*Ntau);
+// shuchud(Ae,(kznumcek*Ntau+kznumphii+dimx*Ntau),((dimx+dimu)*Ntau);
 
 
 
 
 
 
-printf("\n\n...............\n");
-geshu=kznumcik*Ntau+kznumpsii;
+// printf("\n\n...............\n");
+// geshu=kznumcik*Ntau+kznumpsii;
  
 
-shuchud(be,geshu,1);
+// shuchud(be,geshu,1);
 
 
 
-printf("\n\n...............\n");
-shuchud(Ae,(kznumcik*Ntau+kznumpsii),((dimx+dimu)*Ntau+1));
+// printf("\n\n...............\n");
+// shuchud(Ae,(kznumcik*Ntau+kznumpsii),((dimx+dimu)*Ntau+1));
 
 
  
  
  
-printf("\n\n...............\n");
-geshu=(dimx+dimu)*Ntau+1;
-XUk=(double *)malloc(((dimx+dimu)*Ntau+1)*sizeof(double)); shuchud(XUk,geshu,1);
+// printf("\n\n...............\n");
+// geshu=(dimx+dimu)*Ntau+1;
+// XUk=(double *)malloc(((dimx+dimu)*Ntau+1)*sizeof(double)); shuchud(XUk,geshu,1);
 
 
 
