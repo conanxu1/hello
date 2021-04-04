660c0820dfd629006b2f900c44298ba9401616b3
conanxu1 <872144755@qq.com>
2021-04-04 15:32:39 +0800

    ok

--- tools/b站工具/bili_quguan.py
+++ tools/b站工具/bili_quguan.py
@@ -64,116 +64,116 @@ def page_bili(uurl):
 
 
 
-
-def bili():
-	
-
-	jieguo=[]
-
-	#################
-
-	 
-    
-	print("开始批量取关")
-	
-	pp=100
-	
-	driver.get("http://baidu.com")
-	action = ActionChains(driver)
-		
-	time.sleep(300)
-		
-    
-    
-    
-    
-    
-    
-    
-	jieguo=[]
-	
-	
-#############################################	
-	
-	
-	oo=1
-	for i in range(pp):
-		print(pp)
-		try:
-		
-			page = driver.page_source
-			soup=BeautifulSoup(page,'html.parser')
-			
-			
-			if bili_opt==1:
-				array=soup.findAll('h2',attrs={"class":"article-title"})
-			if bili_opt==2:
-				array=soup.findAll('a',attrs={"class":"title"})
-			
-			# print(array)
-			
-			for ee in array:
-			
-				try:
-					
-					
-					
-					if bili_opt==1:
-						jilu=ee.find("a").get("href")
-						
-						jieguo.append("http:"+jilu+'\n')
-					if bili_opt==2:
-						jilu=ee.get("href")
-						print(jilu)
-					
-						jieguo.append("annie -p http:"+jilu+'\n')
-			
-				except:
-					print(ee)
-					print("\n")
-
-				
-			print(i)	
-			print("\n")
-				
-			action.key_down(Keys.SPACE).key_up(Keys.SPACE).perform()
-			time.sleep(5)
-			action.key_down(Keys.SPACE).key_up(Keys.SPACE).perform()	
-			time.sleep(5)
-			if oo<pp:
-				down_btn =  driver.find_element_by_xpath("//li[@class='be-dropdown-item']")
-		
-				time.sleep(5)
-				down_btn.click()
-				oo+=1
-		except:
-			print("....")
-			
-			
-
-
-	if bili_opt==1:
-	
-	
-	
-
-		for ee in jieguo:
-			print(ee)
-			page_bili(ee)
-
-		
-		
-		
-		
-		
-	
-	if bili_opt==2:
-		f=open("bili_vid.sh","w",encoding="utf-8")
-		f.write(" ".join(jieguo))
-		f.close()
-	
-#os.chdir("D:\mygit\hello\tools\b站工具")
-print(os.getcwd())	
+#
+#def bili():
+#	
+#
+#	jieguo=[]
+#
+#	#################
+#
+#	 
+#    
+#	print("开始批量取关")
+#	
+#	pp=100
+#	
+#	driver.get("http://baidu.com")
+#	action = ActionChains(driver)
+#		
+#	time.sleep(300)
+#		
+#    
+#    
+#    
+#    
+#    
+#    
+#    
+#	jieguo=[]
+#	
+#	
+##############################################	
+#	
+#	
+#	oo=1
+#	for i in range(pp):
+#		print(pp)
+#		try:
+#		
+#			page = driver.page_source
+#			soup=BeautifulSoup(page,'html.parser')
+#			
+#			
+#			if bili_opt==1:
+#				array=soup.findAll('h2',attrs={"class":"article-title"})
+#			if bili_opt==2:
+#				array=soup.findAll('a',attrs={"class":"title"})
+#			
+#			# print(array)
+#			
+#			for ee in array:
+#			
+#				try:
+#					
+#					
+#					
+#					if bili_opt==1:
+#						jilu=ee.find("a").get("href")
+#						
+#						jieguo.append("http:"+jilu+'\n')
+#					if bili_opt==2:
+#						jilu=ee.get("href")
+#						print(jilu)
+#					
+#						jieguo.append("annie -p http:"+jilu+'\n')
+#			
+#				except:
+#					print(ee)
+#					print("\n")
+#
+#				
+#			print(i)	
+#			print("\n")
+#				
+#			action.key_down(Keys.SPACE).key_up(Keys.SPACE).perform()
+#			time.sleep(5)
+#			action.key_down(Keys.SPACE).key_up(Keys.SPACE).perform()	
+#			time.sleep(5)
+#			if oo<pp:
+#				down_btn =  driver.find_element_by_xpath("//li[@class='be-dropdown-item']")
+#		
+#				time.sleep(5)
+#				down_btn.click()
+#				oo+=1
+#		except:
+#			print("....")
+#			
+#			
+#
+#
+#	if bili_opt==1:
+#	
+#	
+#	
+#
+#		for ee in jieguo:
+#			print(ee)
+#			page_bili(ee)
+#
+#		
+#		
+#		
+#		
+#		
+#	
+#	if bili_opt==2:
+#		f=open("bili_vid.sh","w",encoding="utf-8")
+#		f.write(" ".join(jieguo))
+#		f.close()
+#	
+##os.chdir("D:\mygit\hello\tools\b站工具")
+#print(os.getcwd())	
 	
 profile = webdriver.FirefoxProfile()
 profile.set_preference("general.useragent.override", "Mozilla/5.0 (Windows NT 6.1; rv:22.0) Gecko/20130405 Firefox/22.0")
@@ -186,13 +186,14 @@ driver = webdriver.Firefox(profile,options=fireFoxOptions)
 driver.get("https://space.bilibili.com/546934523/fans/follow")
 action = ActionChains(driver)
 
-
+input("开始取关")
 
 for i in range(200):
     print(i)
-driver.get("https://space.bilibili.com/546934523/fans/follow")
-driver.execute_script('''$(".be-dropdown-item:contains('取消关注')").click()''')
-time.sleep(10)
+    driver.get("https://space.bilibili.com/546934523/fans/follow")
+    time.sleep(5)
+    driver.execute_script('''$(".be-dropdown-item:contains('取消关注')").click()''')
+    time.sleep(5)
 
 
 
