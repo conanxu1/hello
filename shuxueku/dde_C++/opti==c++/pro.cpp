bcb588e1ad5997b1c1cf80c40dc46fa7c6399356
conanxu1 <872144755@qq.com>
2021-04-04 21:46:01 +0800

    oo

--- shuxueku/dde_C++/opti==c++/pro.cpp
+++ shuxueku/dde_C++/opti==c++/pro.cpp
@@ -131,21 +131,21 @@ void Rn_copy(double* tm,Rn tM,int n)
 
 
 #ifndef noeigen	
-void Rmn_copy(double* tm,Rmn tM,int m,int n)
+void Rmn_copy(double* tm,Rmn *tM,int m,int n)
 {
 	for(int i=0;i<m;i++)
 	for(int j=0;j<n;j++)
-	tM(i,j)=tm[ i*m+j ];
+	*tM(i,j)=tm[ i*m+j ];
 	
 	
 	
-	cout<<endl<<"Ma:"<<tM<<endl;
+	cout<<endl<<"Ma:"<<*tM<<endl;
 }
 
-void Rn_copy(double* tm,Rn tM,int n)
+void Rn_copy(double* tm,Rn *tM,int n)
 {
 	for(int i=0;i<n;i++)
-	tM(i)=tm[i];
+	*tM(i)=tm[i];
 }
 
 #endif
