5b9a70c21dbd91b792c5b56dd1a9968a76c0ff64
conanxu1 <872144755@qq.com>
2021-05-21 07:38:23 +0800

    33

--- tools/ipfav2.py
+++ tools/ipfav2.py
@@ -20,7 +20,7 @@ from email.header import Header
 # 第三方 SMTP 服务  
 mail_host="smtp.163.com"  #设置服务器  
 mail_user="xuwenhan_2018@163.com"    #用户名  
-mail_pass="MIPGOVZGMKLAWMLZ"   #口令,QQ邮箱是输入授权码，在qq邮箱设置 里用验证过的手机发送短信获得，不含空格  
+mail_pass="ARNQTIFRNWXMGVMB"   #口令,QQ邮箱是输入授权码，在qq邮箱设置 里用验证过的手机发送短信获得，不含空格  
 
 
 
