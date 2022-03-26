b9f0af17c61f60a14c415a7d982ad5d879a5e80a
conanxu1 <872144755@qq.com>
2022-03-26 20:21:35 +0800

    fix
    fix

--- tools/file2tiff/f2tiff-v2.py
+++ tools/file2tiff/f2tiff-v2.py
@@ -8,82 +8,61 @@ import time
 import struct
 import sys
 
+
+
+
+#按字节写入
+# f=open(file,"ab")
+# f.write(struct.pack('B', num))
+#f.close()
+	
 def mywrite(file,num):
-
-	# f=open(file,"ab")
-	# f.write(struct.pack('B', num))
-	# f.close()
-	
 	file.write(struct.pack('B', num))
+    
 
-	
-	
-	
-	
-	
-
-	# newhex=hexnum.replace("0x","")
-	# print(newhex)
-	# byarray=bytearray.fromhex(str(newhex))
-	# print(byarray)
-	# for ee in byarray:
-		# print(ee)
-		# file.write(ee)
+ 
 
 
-
+#时间戳
 def timestamp():
 	return str(datetime.datetime.now()).replace(" ","-").replace(":","-").replace(".","-")
 
-# def info2png(urlname):
-	# filesize= os.path.getsize(urlname)
 
 
 
-# i,j,k
-
-def  next(i,j,k,nn):
-	if (i+1)*(j+1)==nn*nn:
-		i=0
-		j=0
-		k=k+1
-	elif (j+1)==nn:
-		i=i+1		
-		j=0
-	else:
-		j=j+1
-	return i,j,k
-		
-def next_ij(i,j,nn):
-	if j==nn-1:
-		i=i+1
-		j=0
-	else:
-		j=j+1
-	return i,j
-
-# def myread(m_ts,i,j,k)
 
 
+
+#文件转图片
+#infosize:文件信息长度默认200
+#file_mat:图片矩阵
+#filesize:文件大小
+#num1:所需矩阵元素个数，奇数时元素个数为num1+num2
+#num2:是否为奇数
+#tonji:记录所转换的字节个数
+
+#ps:数字的utf8码为二位数，逗号的utf8码也为二位数，故可直接删除"0x"
 
 def file2mat(urlname,png_wid,m_ts):
 	global infosize
 	
 	file_mat=np.array(np.uint16(np.zeros((png_wid+1,png_wid)) ))
-	i=0
-	j=0
-	
+
 	filesize= os.path.getsize(urlname)
 	
-	
+	#计算所需元素个数
 	num1=math.floor(filesize/2)
 	print(num1)
 	num2=filesize%2
 	print(num2)
-	
+
+    
+    
+    
 	numsize=png_wid*png_wid
 	 
 	numk=math.ceil( filesize/(2*numsize)   )
+	print("\nnumk:")
 	print(numk)
 	print("=================")
 	f=open(urlname,"rb")
@@ -96,15 +75,14 @@ def file2mat(urlname,png_wid,m_ts):
 		sbm=bytes(strr, encoding = "utf8")  
 		jj=0
 		for ee in sbm:
-			# # # # print(ee)
-			file_mat[  png_wid  ,jj]=int(hex(ee),16)
+			file_mat[  png_wid  ,jj]=ee
 			jj=jj+1
-		
-	
-	
+        
+#sbm=bytes(strr, encoding = "utf8") 直接输出为b'文字'。若for ee in sbm:输出为数字
+#hex(数字)="0x11"，int("0x11"，16)=数字
+
 		print(k)
-		i=0
-		j=0
+		
 		
 		for i in range(png_wid):
 			for j in range(png_wid):
@@ -116,10 +94,13 @@ def file2mat(urlname,png_wid,m_ts):
 				c=0
 				ll=0
 				for x in w:
-					c=c+int(hex(x),16)*(2**(ll) )
+					#print(x)
+					# c=c+int(hex(x),16)*(2**(ll) )
+					c=c+x*(2**(ll) )
 					ll=ll+8
 				file_mat[i,j]=c
-				
+				#若num2=1此循环只执行一次
+                
 				
 
 				
@@ -142,18 +123,18 @@ def file2mat(urlname,png_wid,m_ts):
 	
 	
 	
-	
+	#文件信息 info.tiff------------------------------
 	strr=str(num1)+","+str(num2)+","+str(numk)+","+str(png_wid)+","+urlname+","+m_ts
 	sbm=bytes(strr, encoding = "utf8")  
-	# # # print(sbm)
+ 
 	file_mat=np.array(np.uint16(np.zeros((1,infosize)) ))
 	j=0
 	for ee in sbm:
-		# # # # print(ee)
-		file_mat[0,j]=int(hex(ee),16)
+
+		file_mat[0,j]=ee
 		j=j+1
 		
-	# # # print(file_mat)
+
 	im=Image.fromarray(file_mat)
 	im.save(m_ts+"=info"+".tiff")
 	
@@ -162,6 +143,9 @@ def file2mat(urlname,png_wid,m_ts):
 
 
 
+
+
+#返回图片最后一行的信息
 def endstr(img,png_wid):
 	bb=''
 	for jk in range(png_wid):
@@ -170,13 +154,7 @@ def endstr(img,png_wid):
 		
 
 	nstr=bb.rstrip("0x0").replace("0x","")
-	
- 	# nstr=bb.replace("0x","").rstrip("0")   
-	# nstr=bb.replace("0x00","")
-	# nstr=nstr.replace("0x","")
-    
-    
-    
+
 	try:
 		byarray=bytearray.fromhex(nstr)
 	except:
@@ -196,34 +174,29 @@ def png2file(pngname):
     
     
     
-    #读图
+    #读图--------------
 	img = io.imread(pngname)
 	# # # print(img)
 	
     
+	
+    
+    
+    
+    #解析文件信息--------
 	bb=''
 	for j in range(infosize):
 		addnew=hex(int(img[0,j]))
 		bb=bb+addnew
-		# print(addnew)
 
-	print(bb)
-
-
+    #此处数字和逗号utf8码为两位数
 	nstr=bb.rstrip("0x0").replace("0x","")
-	# nstr=bb.replace("0x","").rstrip("0")
-	
-	# nstr=bb.replace("0x00","")
-	# nstr=nstr.replace("0x","")
-	print(nstr)
-	
-    
+   
 	try:
 		byarray=bytearray.fromhex(nstr)
 	except:
 		byarray=bytearray.fromhex(nstr+"0")
-	# # # print(byarray)
-    
+
     
     
 	infostr=byarray.decode("utf8")
@@ -238,133 +211,75 @@ def png2file(pngname):
 	
 	print(filename)
 	print(numk)
-	print("=============")
+	print("=================")
 #############################################
 
+
+#修改图片文件名
 	dirs=os.listdir("./")
 	for file in dirs:
 		if file.endswith(".tiff"):
-
 				img=io.imread(file)
 				try:
 					infosstr=endstr(img,png_wid)
 					infolist=infosstr.split(",")
 					if infolist[1]==	m_ts	:
-						# print(file)
 						os.rename(file,m_ts+"="+infolist[0]+"="+".tiff")
 				except:
 					oooo=1
 
 
-	
-	
-	# # for kk in range(1,numk+1):
-		# # img=io.imread( )
-		
-		# # img = io.imread(pngname)
-	# # # # # print(img)
-	
-	# # bb=''
-	# # for j in range(infosize):
- 
-		# # addnew=hex(int(img[0,j]))
-		# # bb=bb+addnew
 
-
-
-
-	# # nstr=bb.replace("0x","").rstrip("0")
-	# # # # # print(nstr)
-	# # try:
-		# # byarray=bytearray.fromhex(nstr)
-	# # except:
-		# # byarray=bytearray.fromhex(nstr+"0")
-	# # # # # print(byarray)
-	# # infostr=byarray.decode("utf8")             
-	# # infolist=infostr.split(",")
-	
-	
-###############################################
-	
-	
-	
-	
-	
-	# # # print(len(infolist))
-	
+#恢复的文件名
 	bkname="bk_"+filename
 	f=open(bkname,"wb")
 	
 	
-	
-	i=0
-	j=0
-	# k=1
+ 
 
-
-	# # # # k<numk
+	# # # # k<numk range(1,1)为空
 	count=0
 	for k in range(1,numk):
 		print(k)
 		img = io.imread(m_ts+"="+str(k)+"="+".tiff")
 		for i in range( png_wid):
 			for j in range( png_wid):
-				
-
-				
-				# # print("i+\n")
-				# # print(img[i,j])
-
+ 
 				tmpbyte2= math.floor(  int(img[i,j])/2**8 )  
 				tmpbyte1= math.floor(  int(img[i,j])%2**8 )  
-
-				# print(hex(tmpbyte1))
-				# print(hex(tmpbyte2))
-				
-				# print(hex(img[i,j]))
-
-				# time.sleep(1)
+ 
 
 				mywrite(f,tmpbyte1)
 				mywrite(f,tmpbyte2)
 	
 	n2num1=num1-(numk-1)*png_wid*png_wid
 
+
+
 	numi=math.floor(   n2num1/png_wid  )
+    #多出的行
 	numj=math.floor(   n2num1%png_wid  )
-	
-	# # # print("(*****")
-	# # # print(numi)
-	# # # print(numj)
-	# # # # # # k=numk
+	#多出的列
+ 
+ 
+#k=1时也执行
 	for k in range(numk,numk+1):
+		print(k)    
 		img = io.imread(m_ts+"="+str(k)+"="+".tiff")
 		for i in range( numi):
 			for j in range( png_wid):
-				
-				# # # print("\n[")
-				# # # print(img[i,j])
-				# # # print("]")
-				
-				
-				# # # print(int(img[i,j])/2**8)
+ 
 			
 				tmpbyte2= math.floor(  int(img[i,j])/2**8 )  
 				tmpbyte1= math.floor(  int(img[i,j])%2**8 )  
-				
-				# print(tmpbyte1)
-				# print(tmpbyte2) 
-				
-
-				
+ 
 				mywrite(f,tmpbyte1)
 				mywrite(f,tmpbyte2)
 				
 		for i in range( numi,numi+1):
 			for j in range( numj):
 				
-				# print("ii+++\n")
-				# print(img[i,j])
+
 				tmpbyte2= math.floor(  int(img[i,j])/2**8 )  
 				tmpbyte1= math.floor(  int(img[i,j])%2**8 )  
 
@@ -374,23 +289,14 @@ def png2file(pngname):
 
 		
 		if num2>0:
-			if k<numk:
-				
-				img = io.imread(m_ts+"="+str(numk)+"="+".tiff")
-				
-				tmpbyte=hex(img[0,0])
-				mywrite(f,tmpbyte)
-
-			else:
-
-
-				tmpbyte=math.floor(  int(img[numi,numj])%2**8 )  
-				mywrite(f,tmpbyte)
+			tmpbyte=math.floor(  int(img[numi,numj])%2**8 )  
+			mywrite(f,tmpbyte)
 
 
  
 
 	f.close()	
+	print("ok")    
 		
 	
 	
@@ -399,11 +305,21 @@ def png2file(pngname):
 print("python new.py filename 3000 backup"+"\n"+"python new.py filename.tiff recover"+"最小值200")    
     
 	
+    
+    
+    
+    
+    
 	
 global infosize
 infosize =200
 	
 	
+
+
+
+
+
 
 try:
 	urlname=sys.argv[1]
@@ -412,10 +328,6 @@ try:
 except:
 
 	ooo=1
-
-
- 
-
 
 try:
 	if sys.argv[3]=="backup":
@@ -436,12 +348,34 @@ if sys.argv[2]=="recover":
 	
 	
 	
+#================================================	
 	
 	
+# def info2png(urlname):
+	# filesize= os.path.getsize(urlname)
 	
-	
-	
-	
+# i,j,k
+def  next(i,j,k,nn):
+	if (i+1)*(j+1)==nn*nn:
+		i=0
+		j=0
+		k=k+1
+	elif (j+1)==nn:
+		i=i+1		
+		j=0
+	else:
+		j=j+1
+	return i,j,k
+		
+def next_ij(i,j,nn):
+	if j==nn-1:
+		i=i+1
+		j=0
+	else:
+		j=j+1
+	return i,j
+
+# def myread(m_ts,i,j,k)
 	
 	
 	
